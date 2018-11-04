3a1c69c34683e904fa8febde28c0a5824b0657f3
VeryCleverName3 <38430676+VeryCleverName3@users.noreply.github.com>
2018-11-04 09:38:01 -0500

    Update hexaGameScript.js

--- hexaGameScript.js
+++ hexaGameScript.js
@@ -14,7 +14,7 @@ var ctx = c.getContext("2d");
 ctx.lineWidth = 10;
 var logo = new Image;
 logo.src = "hexalogo.svg";
-ctx.drawImage(logo, 0, 0, s, s);
+setTimeout(function(){ctx.drawImage(logo, 0, 0, s, s);}, 100);
 var hexagons = [];
 var hexImgs = [new Image, new Image, new Image, new Image];
 hexImgs[0].src = "greyhex.svg";
