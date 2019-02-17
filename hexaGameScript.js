218a83fd00f36efc81b33d2a9690be2a483bf6b4
VeryCleverName3 <awesome827740@gmail.com>
2019-02-17 13:25:07 -0500

    Did the things on Quinn's list

--- hexaGameScript.js
+++ hexaGameScript.js
@@ -16,10 +16,16 @@ var flipping = false;
 var ctx = c.getContext("2d");
 ctx.lineWidth = 10;
 var logo = new Image;
+var playButton = new Image;
+playButton.src = "Assets/PlayButton.svg";
 logo.src = "hexalogo.svg";
 setTimeout(function(){
-  ctx.drawImage(logo, 0, 0, s, s);
-  //2 player option
+  ctx.drawImage(logo, 0, -s * (1/6), s, s);
+  ctx.drawImage(playButton, s * (1/4) - ((s / 2) / 2), s * (3/4) - ((s / 2) / 2), s / 2, s / 2);
+  ctx.drawImage(downArrow, s * (3 / 4) - (s / 8), s * (3/4) - ((s / 4) / 2) + (s / 12), s / 4, s / 4);
+  ctx.drawImage(upArrow, s * (3 / 4) - (s / 8), s * (3/4) - ((s / 4) / 2) - (s / 12), s / 4, s / 4);
+  ctx.drawImage(numbers[players], s * (3 / 4) - (s / 10), s * (3/4) - ((s / 5) / 2), s / 5, s / 5);
+  /*//2 player option
   ctx.drawImage(hexImgs[1], 0, s * (3/4), hexSize*s/13, hexSize*s/13);
   ctx.drawImage(hexImgs[2], (hexSize*s/13/3), s * (3/4), hexSize*s/13, hexSize*s/13);
   //3 player option
@@ -31,16 +37,27 @@ setTimeout(function(){
   ctx.drawImage(hexImgs[2], s * (5.8/8) + (hexSize*s/13/3), s * (3/4), hexSize*s/13, hexSize*s/13);
   ctx.drawImage(hexImgs[4], s * (6.1/8), s * (3/4) - (hexSize*s/13/3.4), hexSize*s/13, hexSize*s/13);
   ctx.drawImage(hexImgs[5], s * (6.1/8) - (hexSize*s/13/3), s * (3/4) - (hexSize*s/13/3.4), hexSize*s/13, hexSize*s/13);
+  */
 }, 100);
 var hexagons = [];
-var hexImgs = [new Image, new Image, new Image, new Image, new Image, new Image];
+var hexImgs = [new Image, new Image, new Image, new Image, new Image, new Image, new Image, new Image];
 hexImgs[0].src = "greyhex.svg";
-hexImgs[1].src = "bluehex.svg";
-hexImgs[2].src = "redhex.svg";
+hexImgs[1].src = "Assets/RedHex.svg";
+hexImgs[2].src = "Assets/BlueHex.svg";
 hexImgs[3].src = "darkgreyhex.svg";
-hexImgs[4].src = "yellowhex.png";
-hexImgs[5].src = "greenhex.png";
+hexImgs[4].src = "Assets/YellowHex.svg";
+hexImgs[5].src = "Assets/GreenHex.svg";
+hexImgs[6].src = "Assets/PurpleHex.svg";
+hexImgs[7].src = "Assets/LightBlueHex.svg";
 var colorLoop;
+var numbers = [0, 0, new Image, new Image, new Image, new Image, new Image];
+var upArrow = new Image;
+var downArrow = new Image;
+upArrow.src = "Assets/UpArrow.svg";
+downArrow.src = "Assets/DownArrow.svg";
+for(var i = 2; i < numbers.length; i++){
+  numbers[i].src = "Assets/" + i + ".svg";
+}
 for(var i = 0; i < 91; i++){
   hexagons[i] = [[0, 0], [0, 0], 0, [-1, -1, -1, -1, -1, -1], 0];
 }
@@ -64,19 +81,49 @@ hexagons[84][4] = 3;
 onmousedown = function(e){
   if(started && !flipping) update(e.clientX - ((window.innerWidth-s) / 2), e.clientY);
   if(!started){
-    start();
-    if(e.clientX - ((window.innerWidth-s) / 2) <= s * (1/3)) players = 2;
-    else if(e.clientX - ((window.innerWidth-s) / 2) <= s * (2/3)) players = 3;
-    else players = 4;
+    var x = e.clientX - ((window.innerWidth-s) / 2);
+    var y = e.clientY;
+    if(Math.hypot(y - s * (3/4), x -  s * (1/4)) < s / 12) start();
+    if(Math.hypot(y - (s * (3/4) - (s / 12)), x - (s * (3 / 4))) < s / 50){
+      players++;
+      if(players > 6) players = 2;
+      ctx.clearRect(s * (3 / 4) - (s / 10), s * (3/4) - ((s / 5) / 2), s / 5, s / 5);
+      ctx.drawImage(numbers[players], s * (3 / 4) - (s / 10), s * (3/4) - ((s / 5) / 2), s / 5, s / 5);
+      ctx.drawImage(downArrow, s * (3 / 4) - (s / 8), s * (3/4) - ((s / 4) / 2) + (s / 12), s / 4, s / 4);
+      ctx.drawImage(upArrow, s * (3 / 4) - (s / 8), s * (3/4) - ((s / 4) / 2) - (s / 12), s / 4, s / 4);
+    }
+    if(Math.hypot(y - (s * (3/4) + (s / 12)), x - (s * (3 / 4))) < s / 50){
+      players--;
+      if(players < 2) players = 6;
+      ctx.clearRect(s * (3 / 4) - (s / 10), s * (3/4) - ((s / 5) / 2), s / 5, s / 5);
+      ctx.drawImage(numbers[players], s * (3 / 4) - (s / 10), s * (3/4) - ((s / 5) / 2), s / 5, s / 5);
+      ctx.drawImage(downArrow, s * (3 / 4) - (s / 8), s * (3/4) - ((s / 4) / 2) + (s / 12), s / 4, s / 4);
+      ctx.drawImage(upArrow, s * (3 / 4) - (s / 8), s * (3/4) - ((s / 4) / 2) - (s / 12), s / 4, s / 4);
+    }
   }
 }
 ontouchstart = function(e){
   if(started) update(e.touches[0].clientX - ((window.innerWidth-s) / 2), e.touches[0].clientY);
-  if(!started) {
-    start();
-    if(e.touches[0].clientX - ((window.innerWidth-s) / 2) <= s * (1/3)) players = 2;
-    else if(e.touches[0].clientX - ((window.innerWidth-s) / 2) <= s * (2/3)) players = 3;
-    else players = 4;
+  if(!started){
+    var x = e.touches[0].clientX - ((window.innerWidth-s) / 2);
+    var y = e.touches[0].clientY;
+    if(Math.hypot(y - s * (3/4), x -  s * (1/4)) < s / 12) start();
+    if(Math.hypot(y - (s * (3/4) - (s / 12)), x - (s * (3 / 4))) < s / 50){
+      players++;
+      if(players > 6) players = 2;
+      ctx.clearRect(s * (3 / 4) - (s / 10), s * (3/4) - ((s / 5) / 2), s / 5, s / 5);
+      ctx.drawImage(numbers[players], s * (3 / 4) - (s / 10), s * (3/4) - ((s / 5) / 2), s / 5, s / 5);
+      ctx.drawImage(downArrow, s * (3 / 4) - (s / 8), s * (3/4) - ((s / 4) / 2) + (s / 12), s / 4, s / 4);
+      ctx.drawImage(upArrow, s * (3 / 4) - (s / 8), s * (3/4) - ((s / 4) / 2) - (s / 12), s / 4, s / 4);
+    }
+    if(Math.hypot(y - (s * (3/4) + (s / 12)), x - (s * (3 / 4))) < s / 50){
+      players--;
+      if(players < 2) players = 6;
+      ctx.clearRect(s * (3 / 4) - (s / 10), s * (3/4) - ((s / 5) / 2), s / 5, s / 5);
+      ctx.drawImage(numbers[players], s * (3 / 4) - (s / 10), s * (3/4) - ((s / 5) / 2), s / 5, s / 5);
+      ctx.drawImage(downArrow, s * (3 / 4) - (s / 8), s * (3/4) - ((s / 4) / 2) + (s / 12), s / 4, s / 4);
+      ctx.drawImage(upArrow, s * (3 / 4) - (s / 8), s * (3/4) - ((s / 4) / 2) - (s / 12), s / 4, s / 4);
+    }
   }
   mobile = true;
 }
@@ -99,58 +146,76 @@ function  update(mX, mY){
         case 3:
           flip(i, 5);
           break;
+        case 4:
+          flip(i, 6);
+          break;
+        case 5:
+          flip(i, 7);
+          break;
       }
       //console.log("This is hexagon " + i);
       do {
         changed = !changed;
-        for(var i = 0; i < 91; i++){
-          if(hexagons[i][2] != 0 && hexagons[i][2] != 3){
-            var near = [-1, 0, 0, -1, 0, 0];
-            near[hexagons[i][2]]++;
-            for(var j = 0; j < 6; j++){
-              if(hexagons[i][3][j] != -1){
-                if(hexagons[hexagons[i][3][j]][2] != 0 && hexagons[hexagons[i][3][j]][2] != 3)
-                near[hexagons[hexagons[i][3][j]][2]] += 1;
-              }
-            }
-            var order;
-            switch(turn){
-              case 0:
-                order = [1, 5, 4, 2];
-                break;
-              case 1:
-                order = [2, 1, 5, 4];
-                break;
-              case 2:
-                order = [4, 2, 1, 5];
-                break;
-              case 3:
-                order = [5, 4, 2, 1];
-                break;
-            }
-            for(var k = 0; k < 4; k++){
-              if(near[order[k]] > near[hexagons[i][2]]){
-                flip(i, order[k]);
-                changed = true;
+        var order;
+          switch(turn){
+            case 0:
+              order = [1, 7, 6, 5, 4, 2];
+              break;
+            case 1:
+              order = [2, 1, 7, 6, 5, 4];
+              break;
+            case 2:
+              order = [4, 2, 1, 7, 6, 5];
+              break;
+            case 3:
+              order = [5, 4, 2, 1, 7, 6];
+              break;
+            case 4:
+              order = [6, 5, 4, 2, 1, 7];
+              break;
+            case 5:
+              order = [7, 6, 5, 4, 2, 1];
+              break;
+          }
+          for(var k = 0; k < 6; k++){
+            for(var i = 0; i < 91; i++){
+              if(hexagons[i][2] != 0 && hexagons[i][2] != 3){
+                var near = [-1, 0, 0, -1, 0, 0, 0, 0];
+                near[hexagons[i][2]]++;
+                for(var j = 0; j < 6; j++){
+                  if(hexagons[i][3][j] != -1){
+                    if(hexagons[hexagons[i][3][j]][2] != 0 && hexagons[hexagons[i][3][j]][2] != 3)
+                    near[hexagons[hexagons[i][3][j]][2]]++;
+                  }
+                }
+                if(near[order[k]] > near[hexagons[i][2]]){
+                  flip(i, order[k]);
+                  changed = true;
+                }
               }
             }
           }
-        }
       } while (changed);
       turn++;
       if(turn >= players) turn = 0;
       switch(turn){
         case 0:
-          bgColorFadeTo(0, 0, 255);
-          break;
-        case 1:
           bgColorFadeTo(255, 0, 0);
           break;
+        case 1:
+          bgColorFadeTo(0, 150, 255);
+          break;
         case 2:
-          bgColorFadeTo(255, 200, 0);
+          bgColorFadeTo(255, 255, 0);
           break;
         case 3:
           bgColorFadeTo(0, 255, 0);
+          break;
+        case 4:
+          bgColorFadeTo(255, 0, 255);
+          break;
+        case 5:
+          bgColorFadeTo(0, 255, 255);
           break;
       }
     }
@@ -158,15 +223,17 @@ function  update(mX, mY){
 }
 
 function start(){
-  var logoY = 0;
-  var choicesY = 0;
+  /*
+  var logoY =  -s * (1/6);
+  var choicesY = 0;*/
+  var opacity = 0;
   var startingSequence = setInterval(function(){
-    logoY -= s/100;
+    /*logoY -= s/100;
     choicesY += s/100;
     ctx.fillStyle = "white";
     ctx.clearRect(0, 0, s, s);
     ctx.drawImage(logo, 0, logoY, s, s);
-
+/*
     ctx.drawImage(hexImgs[1], 0, s * (3/4) + choicesY, hexSize*s/13, hexSize*s/13);
     ctx.drawImage(hexImgs[2], (hexSize*s/13/3), s * (3/4) + choicesY, hexSize*s/13, hexSize*s/13);
     //3 player option
@@ -181,8 +248,20 @@ function start(){
     if(logoY < -s){
       drawHexagons();
       started = true;
-      body.style.backgroundColor = "rgba(0, 0, 255, 0.3)";
-      bgColors = [0, 0, 255];
+      body.style.backgroundColor = "rgba(255, 0, 0, 0.3)";
+      bgColors = [255, 0, 0];
+      clearInterval(startingSequence);
+    }
+    */
+    opacity += 0.01;
+    if(opacity > 1) opacity = 1;
+    ctx.fillStyle = "rgba(255, 255, 255, " + opacity + ")";
+    ctx.fillRect(0, 0, s, s);
+    if(opacity >= 0.5 && !started){
+      drawHexagons();
+      started = true;
+      bgColors = [255, 255, 255];
+      bgColorFadeTo(255, 0, 0);
       clearInterval(startingSequence);
     }
   }, 1000/60);
@@ -357,28 +436,40 @@ function flip(hex, nState){
 }
 function bgColorFadeTo(r, g, b){
   clearInterval(colorLoop);
+  var dif = 0;
+  if(Math.abs(r - bgColors[0]) > dif){
+    dif = Math.abs(r - bgColors[0]);
+  }
+  if(Math.abs(g - bgColors[1]) > dif){
+    dif = Math.abs(g - bgColors[1]);
+  }
+  if(Math.abs(b - bgColors[2]) > dif){
+    dif = Math.abs(b - bgColors[2]);
+  }
+  var speed = dif / 10;
+
   colorLoop = setInterval(function(){
     if(bgColors[0] > r){
-      bgColors[0]--;
+      bgColors[0] -= speed;
     }
     if(bgColors[0] < r){
-      bgColors[0]++;
+      bgColors[0] += speed;
     }
     if(bgColors[1] > g){
-      bgColors[1]--;
+      bgColors[1] -= speed;
     }
     if(bgColors[1] < g){
-      bgColors[1]++;
+      bgColors[1] += speed;
     }
     if(bgColors[2] > b){
-      bgColors[2]--;
+      bgColors[2] -= speed;
     }
     if(bgColors[2] < b){
-      bgColors[2]++;
+      bgColors[2] += speed;
     }
     body.style.backgroundColor = "rgba(" + bgColors[0] + ", " + bgColors[1] + ", " + bgColors[2] + ", 0.3)";
     if(bgColors[0] == r && bgColors[1] == g && bgColors[2] == b){
       clearInterval(colorLoop);
     }
-  }, 1);
+  }, 1000/60);
 }
